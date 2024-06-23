264dcb87b6cd7149ba713da8576bf6111b1f3a36
Mritunjay Pandey <mritunjaypandey.ee@gmail.com>
2024-06-23 23:51:23 +0530

    Todays update

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('best_rf_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json(force=True)
+        df = pd.DataFrame(data, index=[0])  # Create DataFrame with a single row index
+        prediction = model.predict(df)
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
